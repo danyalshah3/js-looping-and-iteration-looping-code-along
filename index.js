86e7d13d6f6f5ac8bcf101502caeb26b5dd32abb
danyalshah3 <72283767+danyalshah3@users.noreply.github.com>
2021-07-30 16:08:33 -0400

    commit tree e0de5628289b0c2f35c1d341143e7d007b8f56c9

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solutions in this file
+// const name = ["Ada", "Brendan", "Ali"]
+// const event =  ["birthday"]
+
+function writeCards(name, event) {
+    let messages = []
+    // byebug
+    for (let i = 0; i < name.length; i++) {
+        messages.push(`Thank you, ${name[i]}, for the wonderful ${event} gift!`);
+      }
+      return messages;
+}
